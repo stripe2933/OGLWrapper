2c57267644ef76a5934fff06473c2fb6bc36d4d8
gomkyung2 <stripe2933@gmail.com>
2023-12-01 23:42:30 +0900

    More camera helper functions.

--- include/OGLWrapper/Helper/Camera.hpp
+++ include/OGLWrapper/Helper/Camera.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <cmath>
+
 #include <glm/ext/vector_float3.hpp>
 #include <glm/ext/matrix_float4x4.hpp>
 
@@ -22,5 +24,13 @@ namespace OGLWrapper::Helper::Camera{
 
     static glm::vec3 getUp(const glm::mat4 &inv_view) noexcept {
         return inv_view[1];
+    }
+
+	static float getFov(const glm::mat4 &perspective) noexcept {
+        return 2.f * std::atanf(1.f / perspective[1][1]);
+    }
+
+	static float getAspect(const glm::mat4 &perspective) noexcept {
+        return perspective[1][1] / perspective[0][0];
     }
 }
