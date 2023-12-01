a4ad5ed952b64025eaec7acf2f1c04c990676a70
gomkyung2 <stripe2933@gmail.com>
2023-12-02 02:27:21 +0900

    New object: framebuffer, renderbuffer.

---
+++ include/OGLWrapper/Framebuffer.hpp
@@ -0,0 +1,72 @@
+//
+// Created by gomkyung2 on 12/1/23.
+//
+
+#pragma once
+
+#include <GL/gl3w.h>
+
+#include "Texture.hpp"
+#include "Renderbuffer.hpp"
+#include "gl_constraints.hpp"
+#include "strict_mode.hpp"
+
+namespace OGLWrapper {
+    template <GLenum Target> requires (GLConstraints::isFramebufferTarget<Target>())
+    struct Framebuffer {
+        static constexpr GLenum target = Target;
+
+        GLuint handle;
+
+        Framebuffer() {
+            glGenFramebuffers(1, &handle);
+        }
+        Framebuffer(const Framebuffer&) = delete;
+        Framebuffer(Framebuffer&& source) : handle { source.handle } {
+            source.handle = 0;
+        }
+
+        ~Framebuffer() {
+            if (handle != 0) {
+                glDeleteFramebuffers(1, &handle);
+            }
+        }
+
+        void bind() const {
+            glBindFramebuffer(Target, handle);
+        }
+
+        template <bool CheckAttachment = OGLWRAPPER_STRICT_MODE>
+        void attachTexture(GLenum attachment, const auto &texture /* TODO: add type check */) {
+            if constexpr (CheckAttachment) {
+                if (!GLConstraints::isAttachmentType(attachment)) {
+                    throw std::invalid_argument { "Invalid attachment type." };
+                }
+            }
+
+            bind();
+            glFramebufferTexture2D(Target, attachment, texture.target, texture.handle, 0);
+        }
+
+        template <bool CheckAttachment = OGLWRAPPER_STRICT_MODE>
+        void attachRenderbuffer(GLenum attachment, const Renderbuffer &renderbuffer) {
+            if constexpr (CheckAttachment) {
+                if (!GLConstraints::isAttachmentType(attachment)) {
+                    throw std::invalid_argument { "Invalid attachment type." };
+                }
+            }
+
+            bind();
+            glFramebufferRenderbuffer(Target, attachment, GL_RENDERBUFFER, renderbuffer.handle);
+        }
+
+        bool isComplete() const {
+            bind();
+            return glCheckFramebufferStatus(Target) == GL_FRAMEBUFFER_COMPLETE;
+        }
+
+        static void restoreToDefault() {
+            glBindFramebuffer(Target, 0);
+        }
+    };
+}
---
+++ include/OGLWrapper/Renderbuffer.hpp
@@ -0,0 +1,31 @@
+//
+// Created by gomkyung2 on 12/2/23.
+//
+
+#pragma once
+
+#include <GL/gl3w.h>
+
+namespace OGLWrapper{
+    struct Renderbuffer{
+        GLuint handle;
+
+        Renderbuffer() {
+            glGenRenderbuffers(1, &handle);
+        }
+        Renderbuffer(const Renderbuffer&) = delete;
+        Renderbuffer(Renderbuffer&& source) : handle { source.handle } {
+            source.handle = 0;
+        }
+
+        ~Renderbuffer() {
+            if (handle != 0) {
+                glDeleteRenderbuffers(1, &handle);
+            }
+        }
+
+        void bind() const{
+            glBindRenderbuffer(GL_RENDERBUFFER, handle);
+        }
+    };
+}
